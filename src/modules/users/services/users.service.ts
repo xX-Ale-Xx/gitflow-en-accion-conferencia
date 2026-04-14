fea7170e4e4005afffe4874a4851737df13679a2
Javier Avila <floresjavier092@gmail.com>
2026-04-13 23:37:12 -0600

    fix: replace bcrypt with bcryptjs for improved compatibility

--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { User } from '../entities/user.entity';
 import { CreateUserDto, UpdateUserDto, UserResponseDto } from '../dtos';
 
